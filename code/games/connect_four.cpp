d2c237bd380300b73d4e6a7528339659fc5406aa
Diego Bezerra <dsouzaduarte@gmail.com>
2021-03-12 20:23:21 -0300

    Draw board

--- code/games/connect_four.cpp
+++ code/games/connect_four.cpp
@@ -12,9 +12,21 @@ update_game(Connect_Four_State *state, Game_Input *input) {
 }
 
 internal void
+clear_board(Connect_Four_Board *board) {
+    for (u32 x = 0; x < CONNECT_FOUR_X_COUNT; ++x) {
+        for (u32 y = 0; y < CONNECT_FOUR_Y_COUNT; ++y) {
+            Connect_Four_Tile *tile = &board->tiles[x][y];
+            tile->kind = ConnectFourTileKind_None;
+            tile->x = x;
+            tile->y = y;
+        }
+    }
+}
+
+internal void
 draw_game_view(Connect_Four_State *state) {
     
-    Vector2i dim = state->dimensions;
+    Vector2i dim = state->memory->window_dimensions;
     
     game_frame_begin(dim.width, dim.height);
     
@@ -22,9 +34,81 @@ draw_game_view(Connect_Four_State *state) {
         immediate_begin();
         immediate_quad(0.f, 0.f, (real32) dim.width, (real32) dim.height, make_color(0xff2f3242));
         immediate_flush();
+        
+        draw_board(state);
     } else {
         draw_menu(CONNECT_FOUR_TITLE, state->memory);
     }
+}
+
+internal void
+draw_board(Connect_Four_State *state) {
+    
+    Vector2i dim = state->memory->window_dimensions;
+    
+    u32 max_size = dim.width > dim.height ? dim.height : dim.width;
+    real32 tile_size = max_size * 0.1f;
+    
+    real32 x_width  = CONNECT_FOUR_X_COUNT * tile_size;
+    real32 y_height = CONNECT_FOUR_Y_COUNT * tile_size;
+    
+    real32 sx = center_horizontally((real32) dim.width, x_width);
+    real32 sy = center_vertically((real32) dim.height, y_height);
+    
+    Vector4 board_color = make_color(0xFF7985FF);
+    Vector4 tile_red = make_color(0xFFB01212);
+    Vector4 tile_black = make_color(0xFF222323);
+    Vector4 background = make_color(0xff2f3242);
+    Vector4 tile_black2 = make_color(0xFF000000);
+    
+    immediate_begin();
+    
+    //
+    // Board
+    //
+    {
+        Vector2 min = make_vector2(sx, sy);
+        Vector2 max = make_vector2(min.x + x_width, sy + y_height);
+        immediate_quad(min, max, board_color);
+    }
+    
+    //
+    // Tiles
+    //
+    u32 line = 0;
+    for (u32 x = 0; x < CONNECT_FOUR_X_COUNT; ++x) {
+        for (u32 y = 0; y < CONNECT_FOUR_Y_COUNT; ++y) {
+            Connect_Four_Tile tile = state->board.tiles[x][y];
+            
+            Vector4 color;
+            switch (tile.kind) {
+                case ConnectFourTileKind_Red: {
+                    color = tile_red;
+                } break;
+                case ConnectFourTileKind_Black: {
+                    color = tile_black;
+                } break;
+                default: color = background; break;
+            }
+            
+            real32 start_x = sx + x * tile_size;
+            real32 start_y = sy + y * tile_size;
+            Vector2 center = make_vector2(start_x + tile_size * .5f, start_y + tile_size * .5f);
+            
+            real32 radius = tile_size * 0.44f;
+            immediate_circle_filled(center, radius, color);
+        }
+        
+        if (line < 5) {
+            line++;
+            real32 line_y = sy + line * tile_size;
+            Vector2 min = make_vector2(sx, line_y - 1.f);
+            Vector2 max = make_vector2(min.x + x_width, min.y + 1.f);
+            immediate_quad(min, max, tile_black2);
+        }
+    }
+    
+    immediate_flush();
 }
 
 internal void
@@ -59,8 +143,6 @@ connect_four_game_update_and_render(Game_Memory *memory, Game_Input *input) {
         
         init_game(state);
     }
-    
-    state->dimensions = memory->window_dimensions;
     
     Simulate_Game sim = game_simulate(memory, input, state->game_mode);
     switch (sim.operation) {
--- code/games/connect_four.h
+++ code/games/connect_four.h
@@ -11,17 +11,40 @@ Variant: Different board sizes. Walls inside the board that appear when the spac
 
 #define CONNECT_FOUR_TITLE "Connect Four"
 
+#define CONNECT_FOUR_X_COUNT 7
+#define CONNECT_FOUR_Y_COUNT 6
+
+enum Connect_Four_Tile_Kind {
+    ConnectFourTileKind_None,
+    ConnectFourTileKind_Red,
+    ConnectFourTileKind_Black,
+    ConnectFourTileKind_Count,
+};
+
+struct Connect_Four_Tile {
+    Connect_Four_Tile_Kind kind;
+    u32 x;
+    u32 y;
+};
+
+struct Connect_Four_Board {
+    Connect_Four_Tile tiles[CONNECT_FOUR_X_COUNT][CONNECT_FOUR_Y_COUNT];
+};
+
 struct Connect_Four_State {
     Game_Mode game_mode;
     Game_Memory *memory;
     
-    Vector2i dimensions;
+    Connect_Four_Board board;
 };
 
 internal void init_game(Connect_Four_State *state);
 internal void update_game(Connect_Four_State *state, Game_Input *input);
 
+internal void clear_board(Connect_Four_Board *board);
+
 internal void draw_game_view(Connect_Four_State *state);
+internal void draw_board(Connect_Four_State *state);
 internal void draw_hud(Connect_Four_State *state);
 
 internal void connect_four_menu_art(App_State *state, Vector2 min, Vector2 max);
