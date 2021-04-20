b8668318e09e0cd9f9cefe440bd800466afabb70
Diego Bezerra <dsouzaduarte@gmail.com>
2021-04-20 20:15:53 -0300

    Add boilerplate routines

---
+++ code/games/bejeweled.cpp
@@ -0,0 +1,34 @@
+internal void
+init_game(Bejeweled_State *state) {
+    // TODO(diego): Implement
+}
+
+internal void
+update_game(Bejeweled_State *state, Game_Input *input) {
+    // TODO(diego): Implement
+}
+
+internal void
+draw_game_view(Bejeweled_State *state) {
+    // TODO(diego): Implement
+}
+
+internal void
+bejeweled_menu_art(App_State *state, Vector2 min, Vector2 max) {
+    // TODO(diego): Implement
+}
+
+internal void
+bejeweled_game_restart(Bejeweled_State *state) {
+    // TODO(diego): Implement
+}
+
+internal void
+bejeweled_game_update_and_render(Game_Memory *memory, Game_Input *input) {
+    // TODO(diego): Implement
+}
+
+internal void
+bejeweled_game_free(Game_Memory *memory) {
+    // TODO(diego): Implement
+}
--- code/games/bejeweled.h
+++ code/games/bejeweled.h
@@ -1,1 +1,29 @@
 /* date = April 20th 2021 8:10 pm */
+/*
+ 
+Training 12: Bejeweled
+
+Description: The board is filled with seven different types of jewels. The player can swap two adjacent jewels to form a three-in-a-row, causing the jewels to disappear and the jewels on top of them to fall down. Creating chain reactions gives bonus points.
+
+Variant: Different power ups for matching a particular jewel. Be able to sometimes swap jewels that are not adjacent to each other. Timed games.
+
+*/
+
+#define BEJEWELED_TITLE "Bejeweled"
+
+struct Bejeweled_State {
+    Game_Mode game_mode;
+    Game_Memory *memory;
+    
+    // TODO(diego): Fill
+};
+
+internal void init_game(Bejeweled_State *state);
+internal void update_game(Bejeweled_State *state, Game_Input *input);
+
+internal void draw_game_view(Bejeweled_State *state);
+
+internal void bejeweled_menu_art(App_State *state, Vector2 min, Vector2 max);
+internal void bejeweled_game_restart(Bejeweled_State *state);
+internal void bejeweled_game_update_and_render(Game_Memory *memory, Game_Input *input);
+internal void bejeweled_game_free(Game_Memory *memory);
