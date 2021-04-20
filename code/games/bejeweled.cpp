b385c993ff412b00480ce5c6351beba38aeb0a97
Diego Bezerra <dsouzaduarte@gmail.com>
2021-04-20 20:22:47 -0300

    More boilerplate

--- code/games/bejeweled.cpp
+++ code/games/bejeweled.cpp
@@ -1,6 +1,9 @@
 internal void
 init_game(Bejeweled_State *state) {
-    // TODO(diego): Implement
+    state->game_mode = GameMode_Playing;
+    state->memory->game_mode = GameMode_Playing;
+    
+    platform_show_cursor(true);
 }
 
 internal void
@@ -20,15 +23,51 @@ bejeweled_menu_art(App_State *state, Vector2 min, Vector2 max) {
 
 internal void
 bejeweled_game_restart(Bejeweled_State *state) {
-    // TODO(diego): Implement
+    init_game(state);
 }
 
 internal void
 bejeweled_game_update_and_render(Game_Memory *memory, Game_Input *input) {
-    // TODO(diego): Implement
+    Bejeweled_State *state = (Bejeweled_State *) memory->permanent_storage;
+    if (!memory->initialized) {
+        assert(memory->permanent_storage_size == 0);
+        assert(!memory->permanent_storage);
+        memory->initialized = true;
+        
+        Memory_Index total_memory_size = kilobytes(16);
+        Memory_Index total_available_size = total_memory_size - sizeof(Bejeweled_State);
+        
+        state = (Bejeweled_State *) game_alloc(memory, total_memory_size);
+        state->memory = memory;
+        
+        init_game(state);
+    }
+    
+    Simulate_Game sim = game_simulate(memory, input, state->game_mode);
+    switch (sim.operation) {
+        case SimulateGameOp_Update: {
+            update_game(state, input);
+        } break;
+        
+        case SimulateGameOp_Restart: {
+            bejeweled_game_restart(state);
+        } break;
+        
+        default: break;
+    }
+    
+    //
+    // Draw
+    //
+    draw_game_view(state);
 }
 
 internal void
 bejeweled_game_free(Game_Memory *memory) {
-    // TODO(diego): Implement
+    if (!memory) return;
+    
+    Bejeweled_State *state = (Bejeweled_State *) memory->permanent_storage;
+    if (!state) return;
+    
+    // TODO(diego): If necessary.
 }
