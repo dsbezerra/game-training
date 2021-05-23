ae927209648d2448744980e934931f8e0699f6f6
Diego Bezerra <dsouzaduarte@gmail.com>
2021-05-23 01:21:08 -0300

    Format

--- code/gt_serialization.h
+++ code/gt_serialization.h
@@ -12,8 +12,7 @@ struct Byte_Buffer {
 internal Byte_Buffer make_byte_buffer(u32 capacity = BYTE_BUFFER_DEFAULT_CAPACITY);
 internal void        destroy_byte_buffer(Byte_Buffer *buffer);
 
-internal void byte_buffer_resize(Byte_Buffer *buffer, u32 capacity);
-internal void byte_buffer_put(Byte_Buffer *buffer, void *data, u32 size);
-
+internal void        byte_buffer_resize(Byte_Buffer *buffer, u32 capacity);
+internal void        byte_buffer_put(Byte_Buffer *buffer, void *data, u32 size);
 internal Byte_Buffer byte_buffer_from_file(char *filepath);
-internal b32 byte_buffer_write_to_file(Byte_Buffer *buffer, char *filepath);
+internal b32         byte_buffer_write_to_file(Byte_Buffer *buffer, char *filepath);
